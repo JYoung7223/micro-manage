103f31f974fbc4e8d4e06ac9049a6793fc3a91fd
willh33 <wharrison33@hotmail.com>
2021-02-17 20:48:05 -0700

    added navbar and homepage

---
+++ client/src/containers/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Talk about what this app allows you to do
+ */
+const Homepage = (props) => {
+
+	return (
+		<div className="container">
+			
+		</div>
+	)
+}
+
+export default Homepage;
