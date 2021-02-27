0e368f2e462826f8677c5a0cca8c831190a91cfa
darthNater <blstarwars@live.com>
2021-02-27 09:37:35 -0700

    added checklist backend stuff

--- models/checklist.js
+++ models/checklist.js
@@ -1,19 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const checklistSchema = new Schema({
-  title: { type: String, required: true },
-  owner: { type: String, required: true },
-  reference: String,
-  created_date: { type: Date, default: Date.now },
-  phases: [phaseSchema],
-});
-
-const phaseSchema = new Schema({
-  title: String,
-  tasks: [taskSchema],
-});
-
 const taskSchema = new Schema({
   finalReviewedBy: String,
   finalReviewDate: Date,
@@ -25,6 +12,19 @@ const taskSchema = new Schema({
   template: String,
   lineNumber: Number,
   instruction: String,
+});
+
+const phaseSchema = new Schema({
+  title: String,
+  tasks: [taskSchema],
+});
+
+const checklistSchema = new Schema({
+  title: { type: String, required: true },
+  owner: { type: String, required: true },
+  reference: String,
+  created_date: { type: Date, default: Date.now },
+  phases: [phaseSchema],
 });
 
 const Checklist = mongoose.model("Checklist", checklistSchema);
