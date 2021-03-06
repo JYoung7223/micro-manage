70f86c6321a30680fd227eeb3326003f3edbefa2
darthNater <blstarwars@live.com>
2021-03-06 14:18:31 -0700

    got the checklist pulling in

--- client/src/components/Checklist/Checklist.js
+++ client/src/components/Checklist/Checklist.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect, useRef} from 'react';
 import Table from '../Bootstrap/Table/Table';
 import { DateTime } from "luxon";
-import './Checklist.scss'
+import axios from "axios";
+import './Checklist.scss';
+import { useQuery } from "../../utils/useQuery";
+import {useParams} from "react-router";
 
 //Checklist schema
 const columnHeadings = [
@@ -19,138 +22,31 @@ const columnHeadings = [
 
 const now = DateTime.now().toFormat('MM/dd');
 
-const checklist = {
-	phases: [
-	   {
-		   title: 'Phase 1',
-		   tasks: [
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.01',
-				   template: '02.01',
-				   line: 1,
-				   instruction: 'Clean out the dog house'
-			   },
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.02',
-				   template: '02.02',
-				   line: 2,
-				   instruction: 'Setup sleeping area(pillow blankets, etc.'
-			   },
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.02',
-				   template: '02.02',
-				   line: 2,
-				   instruction: 'Sneak the bucket of ice cream out and hide it under your pillow'
-			   }
-		   ]
-	   },
-	   {
-		   title: 'Phase 2',
-		   tasks: [
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.01',
-				   template: '02.01',
-				   line: 3,
-				   instruction: 'Setup rain cover in case it rains'
-			   },
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.02',
-				   template: '02.02',
-				   line: 4,
-				   instruction: 'Prepare dinner (there\'s dog food somewhere)'
-			   }
-		   ]
-	   },
-	   {
-		   title: 'Phase 3',
-		   tasks: [
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.01',
-				   template: '02.01',
-				   line: 5,
-				   instruction: 'Go to sleep'
-			   },
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.02',
-				   template: '02.02',
-				   line: 6,
-				   instruction: 'Wake up'
-			   },
-			   {
-				   finalReview: '',
-				   finalReviewDate: '',
-				   review: 'JY',
-				   reviewDate: now,
-				   prepared: 'WH',
-				   preparedDate: now,
-				   explanation: '01.02',
-				   template: '02.02',
-				   line: 6,
-				   instruction: 'Bully Nate because you woke up on the wrong side of the lawn and it\'s his fault'
-			   }
-		   ]
-	   },
-	]
-}
-
 /**
  * Loads in the checklist corresponding to the id passed
  * @param {checklistId, ...props}
  * @constructor
  */
-const Checklist = ( {checklistId, ...props} ) => {
+const Checklist = ( {props} ) => {
     //state variables
+	let [checklist, setChecklist] = useState({});
+	let { id } = useParams();
 
-    //useEffects(Lifecycle Methods)
+	useEffect( () => {
+		getChecklists();
+	}, []);
+
+	const getChecklists = async () => {
+		let checklistResponse = await axios.get("/api/checklists/" + id);
+
+		setChecklist(checklistResponse.data);
+	};
+
+
+	//useEffects(Lifecycle Methods)
     /**
      * ComponentDidMount: What should happen when this component is first loaded into the DOM
      */
-    useEffect( () => {
-
-    }, []);
-
     //Other Methods
 
     return (
